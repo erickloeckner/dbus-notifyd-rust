29f4e0385249ade235396cd8ddb58e61aeab87fe
erickloeckner <48753426+erickloeckner@users.noreply.github.com>
2019-12-23 05:25:39 -0500

    Update dbus-notifyd_command.sh

--- dbus-notifyd_command.sh
+++ dbus-notifyd_command.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-# This script is run every time a call comes in. Test the script by running it to insure the audio is coming from the correct 
-# output. The audio device with the aplay command is set with "plughw:0,0", the second sound card is "plughw:1,0", etc. Use
+# This script is run every time a call comes in. Test the script by running it to hear if the audio is coming from the correct 
+# output. The first audio device is set with "plughw:0,0", the second sound card is "plughw:1,0", etc. Use
 # only one of the following commands, whichever one works obviously.
+#
+# aplay works with most distros. Use "aplay -L" to list the devices and find the desired output. My workstation's built in output is this:
+#
+# front:CARD=SB,DEV=0
+#   HDA ATI SB, ALC889 Analog
+#   Front speakers
+#
+# Generally the device that says "front speakers" and is not CARD=Headset is the built in soundcard. The full name is "front:CARD=SB,DEV=0"
+#
+# plughw:0,0 is an alternate way of setting devices, where the first number is the device number and the second is the output. I do not
+# recommend using that method since the devices can be enumerated in a different order.
 
-# This works with most distros. Use "aplay -l" to list the devices. Find the desired device. "card X:" is the first number,
-# and "device X:" is the second number. My workstation shows "card 1: SB [HDA ATI SB], device 0: ALC889 Analog [ALC889 Analog]"
-# so it would be "-D plughw:1,0" to use the built in soundcard.
-aplay -q -D plughw:0,0 ./ring.wav &
+aplay -q -D front:CARD=SB,DEV=0 ./ring.wav &
 
 # Uncomment and use this on new workstations with fedora if aplay is not available. The device name should be correct for the
 # current hardware:
